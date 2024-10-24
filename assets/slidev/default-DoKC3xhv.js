e9ff82252a59b931338fe870c5347dd0bb2deada
Ana Antic <aantic@schueler.hakzell.at>
2024-10-24 09:55:31 +0200

    add presentation

---
+++ assets/slidev/default-DoKC3xhv.js
@@ -0,0 +1,1 @@
+import{o as t,b as o,f as s}from"../modules/vue-BVSQz-Bp.js";import{u as a}from"./context-COej1LDo.js";const r={class:"slidev-layout default"},u={__name:"default",setup(l){return a(),(e,n)=>(t(),o("div",r,[s(e.$slots,"default")]))}};export{u as _};
